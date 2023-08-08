017849ea0534d6e80843967c5a7e1f09b8e82d43
Sean Mbogo <sean.c.mbogo@gmail.com>
2023-08-08 13:26:17 -0400

    Create Numbers.java

---
+++ Numbers.java
@@ -0,0 +1,21 @@
+public class Numbers {
+    public static void main(String[] args) {
+        exerciseFour(16);
+        exerciseFour(12);
+        exerciseFour(21);
+        exerciseFour(5);
+    }
+
+    public static void exerciseFour(int number) {
+        if (number == 16) { // this may not work as expected
+            System.out.println("16!");
+        } else if (number >= 20) {
+            System.out.println("20 or greater!");
+        } else if (number <= 10) {
+            System.out.println("10 or less!");
+        } else {
+            System.out.println("Between 10 and 20!");
+        }
+    }
+
+}
